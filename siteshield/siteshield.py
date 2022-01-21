b8dbbf231220c9f4b268b1129a6d6cafaf219c8b
Stuck <stuckf@dnb.com>
2022-01-21 15:33:07 -0500

    Readme

--- siteshield/siteshield.py
+++ siteshield/siteshield.py
@@ -27,36 +27,40 @@ def handler(event, context):
     if debug: logger.info(f"siteshield.py : Handler : Context Path Received: {path}")
     if debug: logger.info(f"siteshield.py : Handler : secret_arn {secret_arn}")
     if debug: logger.info(f"siteshield.py : Handler : secret_name {secret_name}")
-  
-  if force_cache :
-    contents = s3_get()
-  else :
-    try:
-      get_secret_value_response = boto3.client("secretsmanager").get_secret_value(SecretId=secret_name)
-      get_secret_json = json.loads(get_secret_value_response["SecretString"])
-      client_secret = get_secret_json["client_secret"]
-      host = get_secret_json["host"]
-      access_token = get_secret_json["access_token"]
-      client_token = get_secret_json["client_token"]
-    except ClientError as e:
-      logger.info("ClientError : siteshield.py : Handler : secretsmanager " + e.response['Error']['Code'])
-    except KeyError as e:
-      logger.info("KeyError : Secret requires client_secret, access_token, client_token, and host")
-    try:
-      contents = akamai(get_secret_json)
-    except:
+    if debug: logger.info(f"siteshield.py : Handler : force_cache {force_cache}")
+    
+    if force_cache :
       contents = s3_get()
-  
-  response = {
-    "statusCode": 200,
-    "statusDescription": "200 OK",
-    "isBase64Encoded": False,
-    "body" : contents,
-    "headers": {
-    "Content-Type": "text; charset=utf-8"
+    else :
+      try:
+        get_secret_value_response = boto3.client("secretsmanager").get_secret_value(SecretId=secret_name)
+        get_secret_json = json.loads(get_secret_value_response["SecretString"])
+        client_secret = get_secret_json["client_secret"]
+        host = get_secret_json["host"]
+        access_token = get_secret_json["access_token"]
+        client_token = get_secret_json["client_token"]
+      except ClientError as e:
+        logger.info("ClientError : siteshield.py : Handler : secretsmanager " + e.response['Error']['Code'])
+      except KeyError as e:
+        logger.info("KeyError : Secret requires client_secret, access_token, client_token, and host")
+      try:
+        contents = akamai(get_secret_json)
+      except:
+        contents = s3_get()
+    
+    response = {
+      "statusCode": 200,
+      "statusDescription": "200 OK",
+      "isBase64Encoded": False,
+      "body" : contents,
+      "headers": {
+        "Content-Type": "text; charset=utf-8"
+      }
     }
-  }
-  return response
+    return response
+  else :
+    logger.info(f"siteshield.py : Handler : Error Not HTTP GET Method")
+    return "Error"
 
 def akamai(get_secret_json):
   all_ips = list()
@@ -90,8 +94,8 @@ def s3_put(data):
   result = object.put(Body=byte_data)
 
 def s3_get():
-  if debug: logger.info('siteshield.py : s3_put : Bucket = ' + bucket_name )
+  if debug: logger.info('siteshield.py : s3_get : Bucket = ' + bucket_name )
   s3 = boto3.resource("s3")
   object = s3.Object(bucket_name.split(":")[-1], 'cached.txt')
-  result = object.get().decode("utf-8") 
+  result = object.get()['Body'].read().decode('utf-8')
   return result
